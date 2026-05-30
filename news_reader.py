052a22ba47c86d8be4bcd6a5b02125503325263f
Chaithri <gopalachaithri@gmail.com>
2026-05-30 10:50:00 +0530

    Update news_reader.py

--- news_reader.py
+++ news_reader.py
@@ -1,33 +1,19 @@
-import json
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Load dataset
-with open("dataset.json", "r", encoding="utf-8") as file:
-    news_data = json.load(file)
+from vectorizer import create_vectors
 
-# Extract contents
-documents = [article["content"] for article in news_data]
+from preprocess import preprocess_text
 
-# TF-IDF Vectorization
-vectorizer = TfidfVectorizer(stop_words='english')
 
-tfidf_matrix = vectorizer.fit_transform(documents)
+vectorizer, tfidf_matrix, news_data = create_vectors()
 
-print("News Reader Analysis Chatbot")
-print("Type 'exit' to stop")
 
-while True:
+def get_response(user_query):
 
-    query = input("\nEnter your question: ")
+    query = preprocess_text(user_query)
 
-    if query.lower() == "exit":
-        break
-
-    # Convert query to TF-IDF
     query_vector = vectorizer.transform([query])
 
-    # Cosine Similarity
     similarity_scores = cosine_similarity(
         query_vector,
         tfidf_matrix
@@ -35,6 +21,4 @@ while True:
 
     best_match = similarity_scores.argmax()
 
-    print("\nRelevant News:")
-    print(news_data[best_match]["title"])
-    print(news_data[best_match]["content"])
+    return news_data[best_match]
